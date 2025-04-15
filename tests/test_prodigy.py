0b1eccb6aac19c9b09f98e029b3e99772412b8dd
Rodrigo V. Honorato <r.vargashonorato@uu.nl>
2025-04-15 12:53:26 +0200

    refactor integration test

--- tests/test_prodigy.py
+++ tests/test_prodigy.py
@@ -1,18 +1,16 @@
 import json
 import tarfile
 import tempfile
-import unittest
 from io import BufferedReader, TextIOWrapper
 from os import devnull
 from os.path import basename, dirname, join, splitext
 from pathlib import Path
 from sys import stderr, version_info
 
-import numpy as np
 import pytest
-from Bio.PDB import Structure
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.Residue import Residue
+from Bio.PDB.Structure import Structure
 
 from prodigy_prot.modules.freesasa_tools import stdchannel_redirected
 from prodigy_prot.modules.parsers import validate_structure
@@ -31,6 +29,21 @@ def input_pdb_structure():
     input_f = Path(TEST_DATA, "2oob.pdb")
     parser = PDBParser()
     return parser.get_structure(input_f.stem, input_f)
+
+
+@pytest.fixture
+def compressed_dataset_f():
+    return Path(TEST_DATA, "dataset.tgz")
+
+
+@pytest.fixture
+def expected_dataset_json():
+    return Path(TEST_DATA, "dataset.json")
+
+
+@pytest.fixture
+def prodigy_class(input_pdb_structure):
+    yield Prodigy(struct_obj=input_pdb_structure)
 
 
 def test_calculate_ic(input_pdb_structure):
@@ -89,11 +102,6 @@ def test_analyse_nis():
     assert apolar == 0.0
     assert polar == 100.0
     assert charged == 0.0
-
-
-@pytest.fixture
-def prodigy_class(input_pdb_structure):
-    yield Prodigy(struct_obj=input_pdb_structure)
 
 
 def test_prodigy_predict(prodigy_class):
@@ -184,65 +192,59 @@ def get_data_path(path):
         return join("data", *path.split("/"))
 
 
-# Older integration test
-class ProdigyOutputTest(unittest.TestCase):
-    def test_dataset(self):
-        """
-        Test method to compare prediction for 80 dataset cases with
-            expected values.
-        """
-        # load expected data from json
-        with open(get_data_path("dataset.json")) as fh:
-            expected_data = json.load(fh)
+@pytest.mark.integration
+def test_dataset_prediction(compressed_dataset_f, expected_dataset_json):
+    """
+    Test method to compare prediction for 80 dataset cases with
+        expected values.
+    """
+    # load expected data from json
+    with open(expected_dataset_json) as fh:
+        expected_data = json.load(fh)
 
-        # load dataset PDBs
-        dataset = tarfile.open(get_data_path("dataset.tgz"))
-        parser = PDBParser(QUIET=True)
+    # load dataset PDBs
+    dataset = tarfile.open(compressed_dataset_f)
+    parser = PDBParser(QUIET=True)
 
-        keys_equal = ["AA", "PP", "CC", "AP", "CP", "AC"]
-        diffs = {"ba_val": [], "nis_a": [], "nis_c": []}
+    keys_equal = ["AA", "PP", "CC", "AP", "CP", "AC"]
+    diffs = {"ba_val": [], "nis_a": [], "nis_c": []}
 
-        # run prodigy for each dataset in the PDB
-        for entry in dataset:
-            s_name, s_ext = splitext(basename(entry.name))
-            # skip system files in archive
-            if not s_name.isalnum() or s_ext != ".pdb":
-                continue
-            # chains = expected_data[s_name]["Interacting_chains"]
-            handle = dataset.extractfile(entry)
-            # Wrap filehandle to ensure string file handle in Python 3
-            if version_info[0] >= 3:
-                handle = TextIOWrapper(BufferedReader(handle))  # type: ignore
-            # Suppress gap warnings when parsing structure
-            with stdchannel_redirected(stderr, devnull):
-                s = validate_structure(
-                    parser.get_structure(s_name, handle), selection=["A", "B"]
-                )
-            # Test for structure object
-            self.assertIsInstance(s, Structure.Structure)
-            # instantiate Prdigy object,
-            #  run prediction and retrieve result dict
-            prod = Prodigy(s, selection=["A", "B"])
-            prod.predict()
-            results = prod.as_dict()
-            # check for equality of prdicted interface residues
-            for k in keys_equal:
-                self.assertEqual(results[k], expected_data[s_name][k])
-            # check that NIS and binding afinity values are within 2% of
-            #  expected values and add diffs for summary
-            for k in diffs.keys():
-                delta = abs(results[k] / expected_data[s_name][k] - 1)
-                # assume a difference of less then 2%
-                self.assertAlmostEqual(delta, 0, delta=0.02)
-                diffs[k].append(delta)
+    # run prodigy for each dataset in the PDB
+    for entry in dataset:
+        s_name, s_ext = splitext(basename(entry.name))
+        # skip system files in archive
+        if not s_name.isalnum() or s_ext != ".pdb":
+            continue
+        # chains = expected_data[s_name]["Interacting_chains"]
+        handle = dataset.extractfile(entry)
+        # Wrap filehandle to ensure string file handle in Python 3
+        if version_info[0] >= 3:
+            handle = TextIOWrapper(BufferedReader(handle))  # type: ignore
+        # Suppress gap warnings when parsing structure
+        with stdchannel_redirected(stderr, devnull):
+            s = validate_structure(
+                parser.get_structure(s_name, handle), selection=["A", "B"]
+            )
+        # Test for structure object
+        assert isinstance(s, Structure)
+        # self.assertIsInstance(s, Structure.Structure)
+        # instantiate Prdigy object,
+        #  run prediction and retrieve result dict
+        prod = Prodigy(s, selection=["A", "B"])
+        prod.predict()
+        results = prod.as_dict()
+        # check for equality of prdicted interface residues
+        for k in keys_equal:
+            observed_value = results[k]
+            expected_value = expected_data[s_name][k]
 
-        # summarize and print calculated delta values
-        print("Deltas:")
+            assert observed_value == pytest.approx(expected_value)
+
+        # check that NIS and binding afinity values are within 2% of
+        #  expected values and add diffs for summary
         for k in diffs.keys():
-            print("  {} max: {:.2%}".format(k, max(diffs[k])))
-            print("  {} mean: {:.2%}".format(k, np.mean(diffs[k])))
-
-
-#
-# if __name__ == "__main__":
-#     unittest.main()
+            delta = abs(results[k] / expected_data[s_name][k] - 1)
+            # assume a difference of less then 2%
+            # self.assertAlmostEqual(delta, 0, delta=0.02)
+            assert delta == pytest.approx(0, abs=0.02)
+            diffs[k].append(delta)
